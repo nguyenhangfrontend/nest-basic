2927cd480ef4ed3e11df351ed88db5599d6cd4f3
nthang3 <nthang3@humaxvina.com>
2023-10-03 00:16:20 +0700

    add new code

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,12 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
-
+import { Task } from './task.model';
 @Controller('task')
 export class TaskController {
   constructor(private taskService: TaskService) {
     this.taskService = taskService;
   }
-  hello() {
-    // this.taskService.doSomething()
+
+  @Get()
+  getAllTask(): Task[] {
+    return this.taskService.getAllTask();
+  }
+
+  @Post()
+  createTask(data: Task): Task {
+    const response = this.taskService.createTask(data)
+    return response;
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  public getAllTask() {}
+  getAllTask(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(data: Task) {
+    const body: Task = {
+      ...data,
+      id: uuid(),
+      status: TaskStatus.OPEN,
+    };
+    return body;
+  }
 }
