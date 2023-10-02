5ac30366b1ae67232af5929039a8a7118edffa3b
nthang3 <nthang3@humaxdigital.com>
2023-10-02 17:11:33 +0700

    adding files in task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TaskModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/task/task.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private taskService: TaskService) {
+    this.taskService = taskService;
+  }
+  hello() {
+    // this.taskService.doSomething()
+  }
+}
